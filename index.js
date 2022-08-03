82291a5b80a4f5c6633130f0ec32cee8b63595a1
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:30:54 +0700

    TUGAS 27 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,16 @@
+function gabungKata() {
+    var kalimat1 = "No ";
+    var kalimat2 = "rumah saya ";
+    var kalimat3 = 30;
+    var kalimat4 = "A";
+    hasilGabung1 = kalimat1 + kalimat2 + kalimat3 + kalimat4;
+    hasilKonversi1 = String(hasilGabung1);
+    console.log(hasilKonversi1);
+
+    var kalimat5 = "Jalan pangeran Tirtayasa No ";
+    var kalimat6 = 70;
+    hasilGabung2 = kalimat5 + kalimat6;
+    hasilKonversi2 = String(hasilGabung2);
+    console.log(hasilKonversi2);
+}
+gabungKata()
